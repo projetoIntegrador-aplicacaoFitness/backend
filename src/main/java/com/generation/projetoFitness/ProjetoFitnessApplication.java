2d5cb324925a8b32a0a28b1d8b8c40df42491b2e
Caiqe <cttcaiquegomes@gmail.com>
2024-12-03 11:23:35 -0300

    Projeto Base

---
+++ src/main/java/com/generation/projetoFitness/ProjetoFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetoFitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projetoFitness/ProjetoFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projetoFitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
